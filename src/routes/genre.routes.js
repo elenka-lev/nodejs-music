f8c558f0cd2a4ad2eb92c444a927dc6613b9f576
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2025-12-26 18:05:30 +0200

    fix genres

--- src/routes/genre.routes.js
+++ src/routes/genre.routes.js
@@ -5,8 +5,8 @@ import { getAlbumDetails,  getGenres, getPlaylistsByGenre, getPlaylistTracks, ge
 const router = express.Router();
 
 
-router.get('/genres', getGenres);
-router.get('/genres/:genreId/playlists', getPlaylistsByGenre);
+router.get('/', getGenres);
+router.get('/:genreId/playlists', getPlaylistsByGenre);
 router.get('/playlists/:playlistId/tracks', getPlaylistTracks); 
 router.get('/albums/:albumId', getAlbumDetails); 
 router.get('/tracks/:trackId', getTrackDetails);
